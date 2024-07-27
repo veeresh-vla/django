65b8fd6a6a1b8c464343062e5e365230d89f79e8
veeresh-vla <machaniveeresh55@gmail.com>
2024-07-27 10:59:37 +0530

    update

--- Django_mar/mipro1/testapp/admin.py
+++ Django_mar/mipro1/testapp/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-
+#from testapp.models import ContactInfo,Student,Teacher
+#from testapp.models import ContactInfo1,Student1,Teacher1
+#from testapp.models import Person,Employee,Manager
 # Register your models here.
-from testapp.models import Student,Teacher
-admin.site.register(Student)
-admin.site.register(Teacher)
+
+#admin.site.register(Student)
+#admin.site.register(Teacher)
+#admin.site.register(ContactInfo)
+
+#admin.site.register(Student1)
+#admin.site.register(Teacher1)
+#admin.site.register(ContactInfo1)
+
+#admin.site.register(Person)
+#admin.site.register(Employee)
+#admin.site.register(Manager)
--- Django_mar/mipro1/testapp/models.py
+++ Django_mar/mipro1/testapp/models.py
@@ -1,15 +1,40 @@
 from django.db import models
 
 # Create your models here.
-class ContactInfo(models.Model):
-    name = models.CharField(max_length=30)
-    email = models.EmailField()
-    address = models.CharField(max_length=30)
-    class Meta:
-        abstract = True
-class Student(ContactInfo):
-    rollno = models.IntegerField()
-    marks = models.IntegerField()
-class Teacher(ContactInfo):
-    subject = models.CharField(max_length=30)
-    salary = models.FloatField() 
+
+#1.Abstract Base Class model inheritance:
+#class ContactInfo(models.Model):
+    #name = models.CharField(max_length=30)
+    #email = models.EmailField()
+    #address = models.CharField(max_length=30)
+#   
+#class Student(ContactInfo):
+    #rollno = models.IntegerField()
+    #marks = models.IntegerField()
+#class Teacher(ContactInfo):
+    #subject = models.CharField(max_length=30)
+    #salary = models.FloatField() 
+
+
+#2).Multi table inheritance:
+#class ContactInfo1(models.Model):
+    #name = models.CharField(max_length=30)
+    #email = models.EmailField()
+    #address = models.CharField(max_length=30)
+#class Student1(ContactInfo1):
+    #rollno=models.IntegerField()
+    #marks=models.IntegerField()
+#class Teacher1(ContactInfo1):
+    #subject = models.CharField(max_length=30)
+    #salary = models.FloatField()
+
+#3).Multi level inheritance
+#class Person(models.Model):
+    #name = models.CharField(max_length=30)
+    #age = models.IntegerField()
+#class Employee(Person):
+    #eno = models.IntegerField()
+    #esal = models.FloatField()
+#class Manager(Employee):
+    #exp = models.IntegerField()
+    #team_size = models.IntegerField()
